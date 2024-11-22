87f0f21b67615466517df47cc4b1a66f3d2a45a4
Manda <mandashen@gmail.com>
2024-11-22 15:38:58 +0800

    靜態static方法

--- index.php
+++ index.php
@@ -109,6 +109,49 @@ echo $mycat->jump();
 
 ?>
 
+<H1>靜態宣告</H1>
+<?php
+
+class Dog extends Animal implements Behavior{
+    protected $type='dog';
+    protected $name='Doggy';
+    protected static  $count=0;
+    //static $count=0;
+
+    function __construct($hair_color){
+        $this->hair_color=$hair_color;
+        self::$count++;
+    }
+
+    function bark(){
+        echo $this->name . " is barking";
+    }
+
+    function getFeet(){
+        return $this->feet;
+    }
+
+    static function getCount(){
+        return self::$count;
+    }
+
+    function jump(){
+        echo $this->name . " jumpping 1m";
+    }
+}
+
+echo Dog::getCount();
+
+$dog1=new Dog('brown');
+$dog2=new Dog('black');
+$dog3=new Dog('orange');
+$dog4=new Dog('white');
+$dog5=new Dog('white');
+
+
+echo Dog::getCount();
+?>
+
 
 </body>
 </html>
