8466588246385466eeba96f390e057a2854a1466
Manda <mandashen@gmail.com>
2024-11-26 16:37:53 +0800

    下午增改刪

--- db.php
+++ db.php
@@ -18,8 +18,10 @@ class DB{
      * 2.有條件
      * 3.其他SQL功能
      */
+
     function all(...$arg){
         $sql="SELECT * FROM $this->table";
+        
         if(!empty($arg[0])){
             if(is_array($arg[0])){
 
@@ -41,9 +43,72 @@ class DB{
     }
 
     /**
+     * 用find查
+     */
+    function find($id){
+        $sql="SELECT * FROM $this->table ";
+
+        if(is_array($id)){
+            $where=$this->a2s($id);
+            $sql=$sql . " WHERE ". join(" && ",$where);
+        }else{
+            $sql .= " WHERE `id`='$id' ";
+        }
+        return $this->fetchOne($sql);
+    }
+
+    /**
+     * 用save增、改
+     */
+    function save($array){
+
+        if(isset($array['id'])){
+            // update
+            //update table set `欄位1`='值1',`欄位2`='值2' where `id`='值' 
+            $id=$array['id'];
+            unset($array['id']);
+            $set=$this->a2s($array);
+            $sql ="UPDATE $this->table SET ".join(',',$set)." where `id`='$id'";
+                
+        }else{
+            //insert
+            $cols=array_keys($array);
+            $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
+        }
+        echo $sql;
+        return $this->pdo->exec($sql);
+
+    }
+
+
+    /**
+     * 用delete刪除
+     */
+
+    function del($id){
+        $sql="DELETE FROM $this->table ";
+
+        if(is_array($id)){
+            $where=$this->a2s($id);
+            $sql=$sql . " WHERE ". join(" && ",$where);
+        }else{
+            $sql .= " WHERE `id`='$id' ";
+        }
+
+        echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+
+
+
+    /**
      * 把陣列轉成條件字串陣列
      */
     // 從function toWhere($array){ 改成: a2s(array to string)
+
+    
+
     function a2s($array){
         $tmp=[];
         foreach($array as $key => $value){
@@ -58,12 +123,14 @@ class DB{
 
     function fetchOne($sql){
         // echo $sql;
-        return $this->pdo->query($sql)->fetch();
+        // return $this->pdo->query($sql)->fetch();
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
     
     function fetchAll($sql){
         // echo $sql;
-        return $this->pdo->query($sql)->fetchAll();
+        // return $this->pdo->query($sql)->fetchAll();
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
@@ -85,8 +152,14 @@ $DEPT=new DB('dept');
 // 改成:
 // $dept=$DEPT->all();
 // $dept=$DEPT->all(['id'=>3]);
-$dept=$DEPT->all(" Order by `id` DESC");
+// $dept=$DEPT->all(" Order by `id` DESC");
+$dept=$DEPT->find(['code'=>'404']);
 
+// 用delete
+// $DEPT->del(['code'=>'504']);
+
+// 用save
+$DEPT->save(['code'=>'504','id'=>'10','name'=>'資訊發展部']);
 dd($dept);
 
 
