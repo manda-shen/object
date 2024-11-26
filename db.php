3d93ff13d258126e786aa574648ca7745ab3b6eb
Manda <mandashen@gmail.com>
2024-11-26 15:17:27 +0800

    還未用find時-下午第一堂課

--- db.php
+++ db.php
@@ -11,16 +11,66 @@ class DB{
     }
 
     // 要省略下面的$dept=$DEPT->q("SELECT * FROM dept")
-    function all(){
-        return $this->q("SELECT * FROM $this->table");
+
+    /**
+     * 撈出全部資料
+     * 1.整張資料表
+     * 2.有條件
+     * 3.其他SQL功能
+     */
+    function all(...$arg){
+        $sql="SELECT * FROM $this->table";
+        if(!empty($arg[0])){
+            if(is_array($arg[0])){
+
+                $where=$this->a2s($arg[0]);
+                $sql=$sql . " WHERE ". join(" && ",$where);
+            }else{
+                // $sql=$sql.$arg[0];
+                $sql .= $arg[0];
+
+            }
+        }
+
+        if(!empty($arg[1])){
+            $sql=$sql . $arg[1];
+        }
+
+        // return $this->q("SELECT * FROM $this->table");        
+        return $this->fetchAll($sql);
     }
 
+    /**
+     * 把陣列轉成條件字串陣列
+     */
+    // 從function toWhere($array){ 改成: a2s(array to string)
+    function a2s($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[]="`$key`='$value'";
+        }
+        return $tmp;
+    }
 
-    function q($sql){
+    // function q($sql){
+    //     return $this->pdo->query($sql)->fetchAll();
+    // }
+
+    function fetchOne($sql){
+        // echo $sql;
+        return $this->pdo->query($sql)->fetch();
+    }
+    
+    function fetchAll($sql){
+        // echo $sql;
         return $this->pdo->query($sql)->fetchAll();
     }
+
 }
 
+/* function q($sql){
+    return $this->pdo->query($sql)->fetchAll();
+} */
 
 function dd($array){
     echo "<pre>";
@@ -33,8 +83,11 @@ $DEPT=new DB('dept');
 
 // 原本是:$dept=$DEPT->q("SELECT * FROM dept"); 
 // 改成:
-$dept=$DEPT->all();
+// $dept=$DEPT->all();
+// $dept=$DEPT->all(['id'=>3]);
+$dept=$DEPT->all(" Order by `id` DESC");
 
 dd($dept);
+
 
 ?>
